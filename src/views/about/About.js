38f769292fdff3be0d0b0e28e7c1e7c97f9afe3f
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-02-11 12:25:25 -0600

    cleaned up about section

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -35,25 +35,29 @@ const About = (props) => {
                         <LightText>  
                             In February of 2018 Dailan and Arielle Cooper created a business based on the concept that anyone can change any game, with simply an idea. Cloud5 has been successful because of our affordability, creative freethinkers, youthful excitement, and the drive to overachieve while beating the deadline! When you work with Cloud5, you get access to the most innovative tools and services in the IT industry.
                         </LightText>
+                       
+                        {/* <LightText>
+                        </LightText>
+                        <LightText>
+                        </LightText> */}
+                        {/* <LightText>
+                            Offering services such as websites, apps, and e-commerce stores has been our solution this far, and it’s working. But what else can we be doing? Moving into 2020 we asked ourselves, are we doing everything we can to help our clients fully succeed? we have transitioned into offering new services this year to make Cloud5 the complete package when building your business. Services such as social media management and all other marketing needs are now available. Together, we are creating the future. There is no need to be spending time looking for company after company with different services that will help you, when all you need is Cloud5. You should be spending that time using your passion for your business where it is actually needed. Every client should be growing as fast as we are!
+                        </LightText> */}
+                        <LightText>
+                        Since the beginning we have been focused on finding the answer to every client’s question,
+                        “How can I turn my great idea into a business that makes money?”
+
+                        Cloud5 is no ordinary development agency. We threw the blue print out the window to create a company that brings the passion and motivation back into building a business. We have helped over 300 clients build their dreams into a reality, so the only question left is, how can we help you create your future? 
+
+                        </LightText>
+                        
+                        <header>
+                            <DarkTitle>Meet The Team</DarkTitle>
+                        </header>
                         
                         <LightText>
                             We are based out of the new and fast paced IT paradise of Austin, Texas. But don’t think that we can’t work all over the world, remember that corporate reach? We have worked with clients all over the country, there is no geographical limit on the ideas that can come to life! We have a powerhouse team including CEO Dailan Cooper, CTO Latifah President, CMO Kathryn Domako, and a dedicated team ready to create the future. Our team includes over 60 developers with at least 5 years of experience, a marketing team committed to the vision of each and every client, and a growing sales team ready to overcome every challenge thrown their way.
                         </LightText>
-                        <LightText>
-                            Since the beginning we have been focused on finding the answer to every client’s question,
-                        </LightText>
-                        <LightText>
-                            “How can I turn my great idea into a business that makes money?”
-                        </LightText>
-                        <LightText>
-                            Offering services such as websites, apps, and e-commerce stores has been our solution this far, and it’s working. But what else can we be doing? Moving into 2020 we asked ourselves, are we doing everything we can to help our clients fully succeed? we have transitioned into offering new services this year to make Cloud5 the complete package when building your business. Services such as social media management and all other marketing needs are now available. Together, we are creating the future. There is no need to be spending time looking for company after company with different services that will help you, when all you need is Cloud5. You should be spending that time using your passion for your business where it is actually needed. Every client should be growing as fast as we are!
-                        </LightText>
-                        <LightText>
-                        Cloud5 is no ordinary development agency. We threw the blue print out the window to create a company that brings the passion and motivation back into building a business. We have helped over 300 clients build their dreams into a reality, so the only question left is, how can we help you create your future? 
-
-                        </LightText>
-
-                       
                     </Content>
                     
                 </Wrapper>
