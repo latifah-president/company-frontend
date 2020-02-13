835e31399a1aa5cc3a5d9ea416530ceb01022128
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-02-13 08:40:05 -0600

    fixing build error

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Services from './Services/Services';
+import Services from './Services/Services.js';
 import Advantages from './Advantages/Advantages';
 import FeaturedClients from './FeaturedClients/FeaturedClients';
 import SpecialProjects from './SpecialProjects/SpecialProjects';
