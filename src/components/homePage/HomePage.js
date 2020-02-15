6cf97d71942d179470b44c4fef695fa5709d216b
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-02-15 00:10:51 -0600

    fixing build

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Services from './Services/Services.js';
+import Services from './Services/Services';
 import Advantages from './Advantages/Advantages.js';
 import FeaturedClients from './FeaturedClients/FeaturedClients.js';
 import SpecialProjects from './SpecialProjects/SpecialProjects.js';
